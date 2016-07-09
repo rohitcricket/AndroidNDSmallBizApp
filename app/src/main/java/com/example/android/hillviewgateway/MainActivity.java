fbafaeb5e926d4026fc9209299d07f0f5eddf873
ROHIT GUPTA <rohitgupta@rohits-mbp.myhome.westell.com>
2016-07-09 17:26:44 -0500

    added ScrollView and reduced font and margins for smaller screens.

--- app/src/main/java/com/example/android/hillviewgateway/MainActivity.java
+++ app/src/main/java/com/example/android/hillviewgateway/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.hillviewgateway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
